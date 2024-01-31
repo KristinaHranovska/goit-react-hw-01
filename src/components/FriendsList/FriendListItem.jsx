78c21d244090bf9156a89b507a17db4495dd317d
Kristina <kr.hranovska@gmail.com>
2024-01-31 22:05:25 +0200

    task 2 is ready

---
+++ src/components/FriendsList/FriendListItem.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import css from "./FriendListItem.module.css";
+
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={css.item}>
+      <img className={css.avatar} src={avatar} alt="Avatar" width="48" />
+      <p className={css.name}>{name}</p>
+      <p
+        className={clsx(css["status"], {
+          [css.online]: isOnline,
+          [css.offline]: !isOnline,
+        })}
+      >
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string,
+  isOnline: PropTypes.bool.isRequired,
+};
